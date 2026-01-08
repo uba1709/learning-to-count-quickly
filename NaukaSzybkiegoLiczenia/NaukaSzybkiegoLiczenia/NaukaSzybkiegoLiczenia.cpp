b35919790f8ffba69619be772f5f4f479e8e3e79
jak.u.b.p <130249082+uba1709@users.noreply.github.com>
2026-01-08 13:14:49 +0100

    Add files via upload

--- NaukaSzybkiegoLiczenia/NaukaSzybkiegoLiczenia/NaukaSzybkiegoLiczenia.cpp
+++ NaukaSzybkiegoLiczenia/NaukaSzybkiegoLiczenia/NaukaSzybkiegoLiczenia.cpp
@@ -1,9 +1,9 @@
 ﻿#include <iostream>
-#include <list>
 #include <algorithm>
 #include <random>
 #include <cstdlib>
 #include <Windows.h>
+#include <vector>
 using namespace std;
 
 enum class Operacja {
@@ -19,10 +19,12 @@ int pokazMenu(){
     cout << "1. Dodawanie\n";
     cout << "2. Odejmowanie\n";
     cout << "3. Mnozenie\n";
-    cout << "4. Dzielenie\n";
+    cout << "4. Dzielenie (do dopracowania!) \n";
+	cout << "5. KONIEC\n";
     cout << "Wybor: ";
     cin >> wybor;
 	system("cls");
+    if (wybor == 5) { exit(0); };
     return wybor;
 }   
 
@@ -34,7 +36,7 @@ Operacja wybierzOperacje(int wybor){
     case 4: return Operacja::Dzielenie;
     default:
         cout << "Zly wybor\n";
-        exit(0);
+        pokazMenu();
     }
 	system("cls");
 }
@@ -42,36 +44,33 @@ Operacja wybierzOperacje(int wybor){
 void pobierzUstawienia(int& min, int& max, int& ilosc) {
     cout << "Podaj zakres liczb (min max): \n";
     cin >> min >> max;
+    system("cls");
     cout << "Podaj ilosc zadan: ";
     cin >> ilosc;
 	system("cls");
 }
 
-void wymieszajListe(list<int>& l) {
-    vector<int> v(l.begin(), l.end());
-
+void wymieszajListe(vector<int>& v) {
     random_device rd;
     mt19937 g(rd());
     shuffle(v.begin(), v.end(), g);
-
-    l.assign(v.begin(), v.end());
 }
 
-list<int> utworzListe(int min, int max) {
-    list<int> l;
+vector<int> utworzListe(int min, int max) {
+    vector<int> v;
     for (int i = min; i <= max; i++) {
-        l.push_back(i);
+        v.push_back(i);
     }
-    return l;
+    return v;
 }
 
-void quiz(Operacja op, list<int>& l, int iloscPytan){
+void quiz(Operacja op, vector<int>& v, int iloscPytan){
 	int poprawne = 0;
 	int bledne = 0;
 
-    for (int i = 0; i < iloscPytan && l.size() >= 2; i++) {
-        int a = l.front(); l.pop_front();
-        int b = l.front(); l.pop_front();
+    for (int i = 0; i < iloscPytan && v.size() >= 2; i++) {
+        int a = v[i * 2];
+        int b = v[i * 2 + 1];
         int wynik;
 
         switch (op){
@@ -83,35 +82,40 @@ void quiz(Operacja op, list<int>& l, int iloscPytan){
             break;
         case Operacja::Dzielenie: if (b == 0) {
             wynik = a / b;
+            wynik; round(100);
         }
-        break;
+            break;
         default:
             break;
         }
+
         int odp;
         cout << "Ile to " << a << " " << b << "= ";
         cin >> odp;
+
         if (odp == wynik) { poprawne++; } else { bledne++;}
 		system("cls");
 		cout << "=============\n";
         cout << "\nPoprawne: " << poprawne;
         cout << "\nBledne: " << bledne << "\n";
-		Sleep(500);
+		Sleep(1000);
         system("cls");
     }
 }
 
 int main(){
-	int wybor = pokazMenu();
-    Operacja op = wybierzOperacje(wybor);
-	int min, max, ilosc;
-	pobierzUstawienia(min, max, ilosc);
-    list<int> l = utworzListe(min, max);
-    wymieszajListe(l);
+    while (true) {
+        int wybor = pokazMenu();
+        Operacja op = wybierzOperacje(wybor);
+        int min, max, ilosc;
+        pobierzUstawienia(min, max, ilosc);
+        vector<int> v = utworzListe(min, max);
+        wymieszajListe(v);
 
-    int maxPytan = l.size() / 2;
-    if (ilosc > maxPytan)
-        ilosc = maxPytan;
-	quiz(op, l, ilosc);
+        int maxPytan = v.size() / 2;
+        if (ilosc > maxPytan)
+            ilosc = maxPytan;
+        quiz(op, v, ilosc);
+    }
     return 0;
 }
